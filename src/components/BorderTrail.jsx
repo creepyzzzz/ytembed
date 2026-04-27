20c91cf133f8dc520d572a56c6bbe3495f9531c4
creepyzzz <tariqmir1278@gmail.com>
2026-04-27 14:59:58 +0530

    go

--- src/components/BorderTrail.jsx
+++ src/components/BorderTrail.jsx
@@ -1,33 +1,19 @@
-import { motion } from 'framer-motion';
 import { cn } from '../lib/utils';
 
 export function BorderTrail({
   className,
   size = 60,
-  transition,
-  onAnimationComplete,
   style,
 }) {
-  const defaultTransition = {
-    repeat: Infinity,
-    duration: 5,
-    ease: 'linear',
-  };
-
   return (
     <div className='border-trail-mask'>
-      <motion.div
-        className={cn('border-trail-chip', className)}
+      <div
+        className={cn('border-trail-chip border-trail-animation', className)}
         style={{
           width: size,
           offsetPath: `rect(0 auto auto 0 round ${size}px)`,
           ...style,
         }}
-        animate={{
-          offsetDistance: ['0%', '100%'],
-        }}
-        transition={transition || defaultTransition}
-        onAnimationComplete={onAnimationComplete}
       />
     </div>
   );
